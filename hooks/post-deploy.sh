7890ca36f80b131243272af90412b64751a91933
Lizzi Slivinski <eSlivinski@gmail.com>
2018-07-14 17:20:46 -0400

    symlinks

--- hooks/post-deploy.sh
+++
@@ -1,6 +0,0 @@
-cd ../
-
-rm -rf media
-rm -rf styles
-ln -s ../vetro-main/www/static/styles styles
-ln -s ../vetro-main/www/static/media media
--- hooks/pre-deploy.sh
+++
@@ -1,9 +0,0 @@
-cd ../
-
-rm -rf media
-rm -rf styles
-cp -rf ../vetro-main/www/static/styles styles
-cp -rf ../vetro-main/www/static/media media
-
-sed -i -e "s/\/media\/fonts/media\/fonts/g" styles/app.css
-sed -i -e "s/media\/fonts/..\/media\/fonts/g" styles/app.css
